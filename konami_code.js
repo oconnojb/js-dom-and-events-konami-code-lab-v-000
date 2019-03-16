bccdce9051fc5065177f34c0f14baf6639aca885
John O'Connor <oconnojb@gmail.com>
2019-03-16 19:46:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,29 +5,26 @@ function init() {
   const body = document.querySelector('body');
   console.log(body);
 
-  body.addEventListener('keydown', onKeyDown(e))
+  let index = 0;
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    console.log(key);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
 
 init();
-
-let index = 0;
-function onKeyDown(e)
-{
-  const key = parseInt(e.detail || e.which);
-  console.log(key);
-  
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
 
 /*
 // Key codes for A, B, and C keys.
