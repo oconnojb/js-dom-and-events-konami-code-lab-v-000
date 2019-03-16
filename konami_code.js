ae41fa369d56ff927fba9ad24e765b78d5c48ede
John O'Connor <oconnojb@gmail.com>
2019-03-16 19:43:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const body = document.getElementsByTagName('body');
+  const body = document.querySelector('body');
   console.log(body);
 }
+
+init();
 
 /*
 // Key codes for A, B, and C keys.
